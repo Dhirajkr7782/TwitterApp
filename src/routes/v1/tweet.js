9c6520224c05ff06b7ae65fa3fef5d46cbff4425
Dhiraj Yadav <dkydhiraj02@gmail.com>
2025-06-29 20:50:18 +0530

    Adding image uploding in aws s3

--- src/routes/v1/tweet.js
+++ src/routes/v1/tweet.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { getTweets, getTweetById, createTweet} from '../../controllers/tweetController.js';
 import validate from '../../validators/zodValidator.js';
 import { tweetZodSchema } from '../../validators/tweetZodSchema.js';
+import { s3Uploder} from '../../config/multerConfig.js';
 
 const router = express.Router(); //create a new router object
 
@@ -12,6 +13,6 @@ router.get('/', getTweets); // similar to get put post in app.get it also work i
 
 router.get('/:id', getTweetById); // similar to get put post in app.get it also work in this manner 
 
-router.post('/', validate(tweetZodSchema), createTweet);
+router.post('/',s3Uploder.single('tweetImage'), validate(tweetZodSchema), createTweet);
     
 export default router;
